7871634a0cdb6c9078858bacda211eae2b365951
Adil <adilhussain_nitskm@yahoo.in>
2020-05-19 21:04:09 +0530

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       delete '/destroy/:id', to: 'recipes#destroy'
     end
   end
+  root 'homepage#index'
   get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
